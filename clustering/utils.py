1b60b0bc20a23c80dd1013ddf086d16600289b0b
Eric Bridgeford <ebridge2@jhu.edu>
2017-05-03 08:24:28 -0400

    updates I made

--- clustering/utils.py
+++ clustering/utils.py
@@ -22,19 +22,21 @@ def plot_confusion(conf, norm_conf, true_classes,
     """
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    res = ax.imshow(norm_conf, cmap=cmap, interpolation='nearest')
+    res = ax.imshow(norm_conf, cmap=cmap, interpolation='nearest',
+                    aspect='auto')
     ntrue, npred = conf.shape
     for i in xrange(ntrue):
         for j in xrange(npred):
             ax.annotate(str(conf[i, j]), xy=(j, i),
                         horizontalalignment='center',
                         verticalalignment='center')
-    plt.xticks(range(ntrue), true_classes.astype(str), rotation=45)
-    plt.yticks(range(npred), pred_classes.astype(str))
-    ax.set_ylabel('Predicted Label')
-    ax.set_xlabel('True Label')
+    plt.yticks(range(ntrue), true_classes)
+    plt.xticks(range(npred), pred_classes)
+    ax.set_xlabel('Predicted Label')
+    ax.set_ylabel('True Label')
     ax.set_title('Confusion Matrix')
     cb = fig.colorbar(res)
+    fig.tight_layout()
     return fig
 
 def confusion_matrix(M, D):
@@ -58,13 +60,13 @@ def confusion_matrix(M, D):
     cmtx = np.zeros((len(true_classes), len(pred_classes)))
     # pmtx shows the confusion matrix normalized
     pmtx = np.zeros((len(true_classes), len(pred_classes)))
-    for i, true in enumerate(true_classes):
+    for i, pred in enumerate(pred_classes):
         # get the indices that should be labelled
         # true
-        Dcol = D[M == true]
-        n = len(Dcol)
-        for j, pred in enumerate(pred_classes):
-            cmtx[i, j] = np.sum(Dcol == pred)
+        Mcol = M[D == pred]
+        n = len(Mcol)
+        for j, true in enumerate(true_classes):
+            cmtx[i, j] = np.sum(Mcol == true)
             pmtx[i, j] = cmtx[i, j]/float(n)
     fig_conf = plot_confusion(cmtx, pmtx, M, D)
     return (cmtx, pmtx, fig_conf)
@@ -80,7 +82,7 @@ def purity(M, D):
             - the predicted labels.
     """
     (cmtx, pmtx, fig_conf) = confusion_matrix(M, D)
-    purity = np.sum(cmtx.max(axis=0))/float(np.sum(cmtx))
+    purity = np.sum(cmtx.max(axis=1))/float(np.sum(cmtx))
     return (purity, cmtx, pmtx, fig_conf)
 
 def plot_laplacian(L, cmap=plt.cm.jet):
@@ -94,7 +96,12 @@ def plot_laplacian(L, cmap=plt.cm.jet):
     """
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    res = ax.imshow(L, cmap=cmap, interpolation='nearest')
+    minval = np.percentile(L, 0)
+    # since the diagonal will be very significant, this
+    # will eliminate it from our plots
+    maxval = np.percentile(L, 95)
+    res = ax.imshow(L, cmap=cmap, interpolation='nearest', vmin=minval,
+                    vmax=maxval)
     ax.set_ylabel('Training Example')
     ax.set_xlabel('Training Example')
     ax.set_title('Graph Laplacian')
